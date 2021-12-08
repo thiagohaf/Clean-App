273571c83ded55127492027a7c54d4a85296f6e3
thferreira_codestudio <thferreira@codestudio.com.br>
2021-12-08 09:33:30 -0300

    move production code to their own files

---
+++ CleanApp/Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Thiago Henrique Alves Ferreira on 08/12/21.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
