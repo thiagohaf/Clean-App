1c9f922afc7a0ba37f63bc11a23583dfec1e3a0f
thferreira_codestudio <thferreira@codestudio.com.br>
2021-12-08 10:21:51 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ CleanApp/Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Thiago Henrique Alves Ferreira on 08/12/21.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ CleanApp/Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Thiago Henrique Alves Ferreira on 08/12/21.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
