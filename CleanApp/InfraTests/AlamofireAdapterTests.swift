dd600d23ac4f36ddc57d0deeed37227ba046de6a
thferreira_codestudio <thferreira@codestudio.com.br>
2021-12-13 21:11:35 -0300

    add Infra layer

---
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Thiago Henrique Alves Ferreira on 13/12/21.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+
+
+    func test_() {
+    }
+
+}
